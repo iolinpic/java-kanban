5cdde7e2c9a3f717781d63f4864823f4b91fb672
iolinpic <iolinpic@gmail.com>
2024-05-28 09:56:29 +0300

    набор проверок

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import models.Epic;
 import models.SubTask;
 import models.Task;
+import models.TaskStatus;
 
 public class Main {
 
@@ -11,6 +12,7 @@ public class Main {
         Task task2 = new Task(tm.getNextIndex(), "задача 2", "делай дело 2");
         tm.addTask(task1);
         tm.addTask(task2);
+        System.out.println(tm.getTasks());
         boolean res1 = tm.getTasks().size() == 2 && tm.getTasks().containsValue(task1) && tm.getTasks().containsValue(task2);
         if (res1) System.out.println("TASK:добавление, вывод списка - ок");
         tm.deleteTask(task2.getId());
@@ -22,16 +24,31 @@ public class Main {
         tm.updateTask(task1);
         boolean res4 = tm.getTask(task1.getId()).equals(task1);
         if (res4) System.out.println("TASK:обновление - ок");
+        System.out.println(tm.getTasks());
         // Epic functions test
         Epic epic = new Epic(tm.getNextIndex(), "Эпик 1", "делай большое дело 1");
         Epic epic2 = new Epic(tm.getNextIndex(), "Эпик 2", "делай большое дело 1");
         SubTask subTask1 = new SubTask(tm.getNextIndex(), "подзадача 1", "делай маленькое дело 1", epic);
         SubTask subTask2 = new SubTask(tm.getNextIndex(), "подзадача 2", "делай маленькое дело 2", epic2);
         SubTask subTask3 = new SubTask(tm.getNextIndex(), "подзадача 3", "делай маленькое дело 3", epic2);
-        tm.addTask(subTask1);
-        tm.addTask(subTask2);
-        tm.addTask(subTask3);
-        tm.addTask(epic);
-        tm.addTask(epic2);
+        tm.addSubTask(subTask1);
+        tm.addEpic(epic2);
+        System.out.println(tm.getSubTasks());
+        System.out.println(tm.getEpics());
+        subTask1.setStatus(TaskStatus.IN_PROGRESS);
+        tm.updateSubTask(subTask1);
+        System.out.println(tm.getSubTasks());
+        System.out.println(tm.getEpics());
+        tm.deleteSubTask(subTask3.getId());
+        subTask2.setStatus(TaskStatus.DONE);
+        System.out.println(tm.getEpics());
+        System.out.println(tm.getEpicSubTasks(tm.getEpic(epic2.getId())));
+        tm.deleteEpic(epic.getId());
+        System.out.println(tm.getEpics());
+        System.out.println(tm.getSubTasks());
+        tm.deleteSubTask(subTask2.getId());
+        System.out.println(tm.getEpics());
+        System.out.println(tm.getSubTasks());
+
     }
 }
